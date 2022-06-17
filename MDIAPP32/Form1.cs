28ddfa1e181a44b772f365be940f6f61c8880251
Gihun Ham <ham@e-mirim.hs.kr>
2022-06-17 10:20:48 +0900

    #1 MDI, 모달, 모달리스, 메시지박스

--- MDIAPP32/Form1.cs
+++ MDIAPP32/Form1.cs
@@ -15,6 +15,34 @@ namespace MDIAPP32
         public Form1()
         {
             InitializeComponent();
+            //IsMdiContainer = true;
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("내용");
+
+            DialogResult result;
+            do
+            {
+                result = MessageBox.Show("내용", "제목", MessageBoxButtons.RetryCancel, MessageBoxIcon.Warning);
+
+            } while (result == DialogResult.Retry);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            CustomForm form = new CustomForm();
+            //form.MdiParent = this;
+            form.Show();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            CustomForm form = new CustomForm();
+            //form.MdiParent = this;   // 모달창은 MDI로 사용 불가!
+            form.ShowDialog();
         }
     }
 }
